c6a90334738f1a6a9c376ec3ae6bafd445f1819d
Ɩųɱiཞყҳ <lukasjur258@gmail.com>
2026-03-22 23:48:39 +0100

    Side_bar fix

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
 function toggleTheme(){
-document.body.classList.toggle("light");
+  document.body.classList.toggle("light");
 }
 
 function toggleMenu(){
-document.getElementById("sidebar").classList.toggle("active");
+  document.getElementById("sidebar").classList.toggle("active");
 }
 
-const sections = document.querySelectorAll('.section-title');
-sections.forEach(title => {
-  title.addEventListener('click', () => {
-    const section = title.parentElement;
-    section.classList.toggle('open');
-  });
-});
+function initSidebar() {
+  const sections = document.querySelectorAll('.section-title');
 
+  sections.forEach(title => {
+    title.addEventListener('click', () => {
+      const section = title.parentElement;
+      section.classList.toggle('open');
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  fetch("/style/side_bar.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("sidebar").innerHTML = data;
+      initSidebar();
+    });
+
+});
